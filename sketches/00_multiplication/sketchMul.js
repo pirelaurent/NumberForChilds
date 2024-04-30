272632f7558a4b703edecac067c8eb1fc753dc17
pirla <pi.r.laurent@gmail.com>
2024-05-01 00:26:29 +0200

    full sheet with random

--- sketches/00_multiplication/sketchMul.js
+++ sketches/00_multiplication/sketchMul.js
@@ -1,3 +1,5 @@
+let percentageOfEmpty = 0;
+
 let img;
 let crinola = [
   //"#e6194B",
@@ -46,7 +48,6 @@ function draw() {
     let size_y = tailleBase + ky * increment;
     stroke(crinola[ky]);
     let start_x = 0;
-
     // repères horizontaux
     {
       let debut = start_y; // si on veut s'arrêter au début
@@ -64,17 +65,16 @@ function draw() {
       pop();
     }
 
-    for (let kx = ky; kx < 10; kx++) {
+    for (let kx = 0; kx < 10; kx++) {
       let size_x = tailleBase + kx * increment;
-
       // repères verticaux
       if (ky == 0) {
         let middle_x = start_x + size_x / 2;
         stroke(crinola[kx]);
         let upY = start_y + kx * increment;
-        line(middle_x, start_y - 30, middle_x, middle_x);
+        line(middle_x, start_y - 30, middle_x, 980);
         circle(middle_x, -50, 30);
-        // draw value
+        // draw value ----------
         push();
         scale(1, -1);
         textSize(20);
@@ -89,9 +89,36 @@ function draw() {
       //rect(start_x + start_y, start_y, size_x, size_y);
       // Relative to a box , a circle center is at the middle of the box
       let middle_x = start_x + start_y + size_x / 2;
-      fill(240);
-      ellipse(middle_x, start_y + size_y / 2, size_x - 8, size_y - 8);
 
+      let result = (kx + 1) * (ky + 1);
+
+      if (kx == ky) {
+        strokeWeight(4);
+        fill(210);
+        rect(start_x, start_y, size_x - 8, size_y - 8);
+      } else {
+        strokeWeight(2);
+        fill(240);
+        ellipse(
+          start_x + size_x / 2,
+          start_y + size_y / 2,
+          size_x - 8,
+          size_y - 8
+        );
+      }
+      // draw value
+      push();
+      scale(1, -1);
+
+      fill(220);
+      strokeWeight(4);
+      textSize(20 + result / 2);
+      // option random 
+  print(int(random(100)),(int(random(100))>percentageOfEmpty))
+      if (int(random(100))>percentageOfEmpty){
+      text(result, start_x + size_x / 2, -start_y - size_y / 2);
+      }
+      pop();
       start_x += size_x;
     }
     start_y = start_y + size_y;
